5edbf427fa28b98165fd924727bdb6d00a857011
2022-0621 <2022-0621@LAB1-PC10>
2025-05-08 17:18:48 +0800

    Add project files.

---
+++ StudentInformation/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace StudentInformation.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string? FirstName { get; set; }
+        public string? MiddleName { get; set; }
+        public string? LastName { get; set; }
+        public string? Gender { get; set; }
+        public int Age { get; set; }
+    }
+}
